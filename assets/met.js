95ae238ce368cdef4bb0dc7854234278bdd12f1b
Mykneeisonfire <108244837+Mykneeisonfire@users.noreply.github.com>
2022-08-18 20:01:41 -0400

    working on implementing the randomized portion of the code

--- assets/met.js
+++ assets/met.js
@@ -1,23 +1,34 @@
-document.querySelector(".imgDisplay")
-
 var apiUrl =("https://collectionapi.metmuseum.org/public/collection/v1")
 
-var metImg
+var randomArrayItem
 
 
-
-//var rand = randObj[~~(Math.random() * randObj.length)];
-
+//Fetch the Object IDs of the images.//
 
 function fetchObjects (data) {
     fetch (apiUrl +"/search?hasImages=true&isPublicDomain=true&q=paintings")
         .then(res => res.json())
         .then(data => console.log(data))
+        .then(data => randomArrayItem => data);
+
+
+        randomObject(data);
+
+        fetchImage(data);
 }
 
+//Select a Random object from the var array//
 
-function fetchImage (data) {
-    fetch (apiUrl +"/objects/229569")
+function randomObject (data){
+    let randomArrayItemSelection = randomArrayItem[Math.floor(Math.random() * randomArrayItem.length)];
+    console.log (randomArrayItemSelection);
+    
+}
+
+//Fetch Image from the Object ID//
+
+function fetchImage (_data) {
+    fetch (apiUrl +"/objects/448160")
         .then(res => res.json())
         .then(data => {console.log(data)
         
@@ -25,20 +36,26 @@ function fetchImage (data) {
         })
      }
 
+//Display the selected image and the relevant information//
+
 function imgDisplay(data) {
     var img = data.primaryImage;
     var artistName = data.artistDisplayName;
     var artTitle = data.title;
+    var objectUrl = data.objectURL;
 
     document.querySelector("#apiImg").src = img;
     document.querySelector(".artistName").innerText = artistName;
-    document.querySelector(".credits").InnerText = artTitle;
+    document.querySelector(".artTitle").innerText = artTitle;
+   
+    //clicking on the image will bring you to the url//
+
+    $("#apiImg").on('click', function(){
+        window.location = objectUrl;    
+    });
 }
 
-
-
-
-//document.addEventListener("DOMContentLoaded",fetchObjects());
+document.addEventListener("DOMContentLoaded",fetchObjects());
 document.addEventListener("DOMContentLoaded",fetchImage());
 
 
