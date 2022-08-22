e28090ee7e3a554357600ab553e4466a2fa3ddf8
Dayne Lalmond <dlalmondo@gmail.com>
2022-08-22 16:17:43 -0400

    user can now add their input to any div element

--- assets/script.js
+++ assets/script.js
@@ -22,6 +22,8 @@ function getDays() {
         article.textContent = dayjs([month, i]).format('MMM, DD')
         calendar.append(div);
         div.append(article);
+        // the div elements are created for every day
+        // the article elements are attached inside the divs
         showArticle(article, i)
         showDiv(div, i)
     }
@@ -40,14 +42,30 @@ function showArticle(article, i) {
 function showDiv(div, i) {
   
   if (i === date) {
-      div.classList.add('notification', 'is-danger', 'is-light')
-      check(div)}
+      div.classList.add('notification', 'is-success', 'label')
+      div.classList.add('custom-highlight')
+      }
+      if (i < date) {
+        div.remove();
+        }
   else {
-      div.classList.remove('notification', 'is-danger', 'is-light')}
+      div.classList.add('notification', 'is-primary', 'is-light')}
+// the function below is called at the end of the if statements to correct the characteristics within the element
+  check(div)
 }
 
 function check(div) {
-  var doc = document.createElement("li")
-  doc.classList.add('content', 'is-small')
-  div.append(doc)
+  // if (enabled == true) {
+    // this function allows the user to modify the calendar with the use of the text form and clicking on divs
+    div.addEventListener("click", function() {
+      var doc = document.createElement('li')
+      var toDo = document.querySelector("#todo")
+      doc.classList.add('content', 'is-small')
+      doc.textContent = toDo.value
+      div.append(doc)
+  })
 }
+//   else {
+//     console.log('asdasfd')
+//   }
+// }
