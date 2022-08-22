cb31066ec9818012cf6a5ea06a2dba41ce3d61c7
Mykneeisonfire <108244837+Mykneeisonfire@users.noreply.github.com>
2022-08-22 00:39:31 -0400

    finally working in some form

--- assets/met.js
+++ assets/met.js
@@ -1,51 +1,29 @@
 var apiUrl =("https://collectionapi.metmuseum.org/public/collection/v1");
 
-let randomArray = [];
+const randomArray = [];
+
+//Selects a random ObjectID from the array to use for the image.//
+let randomId = [Math.floor(Math.random() * 39273)];
 
 
 //Fetch the Object IDs of the images.//
 
 async function fetchObjects () {
- 
 
-    fetch (apiUrl +"/search?hasImages=true&isPublicDomain=true&q=paintings")
+     await fetch (apiUrl +"/search?hasImages=true&isPublicDomain=true&q=paintings")
         .then (res => res.json())
-        .then (data => { randomArray.push(data) });
-        console.log (randomArray)
-
-   
-    
-        
-     
-}
-      
-      
-        
-
-
-
-
-
-
-async function selectRandomId() {
-    await fetchObjects();
-        var randomId = Math.floor(Math.random() * randomArray.length)
-
+        .then (data => {randomArray.push(data.objectIDs) });
+        console.log (randomArray);
         console.log(randomId);
-        
-      
-        
 
-       
-
-}    
-
+    fetchImage();
+}
 
 //Fetch Image from the Object ID//
 
-async function fetchImage (_data) {
-    await selectRandomId();
-        fetch (apiUrl +"/objects/" +randomId)
+ function fetchImage () {
+    
+        fetch (apiUrl +"/objects/" + randomId)
     
             .then(res => res.json())
             .then(data => {console.log(data)
@@ -56,24 +34,24 @@ async function fetchImage (_data) {
 
 //Display the selected image and the relevant information//
 
-function imgDisplay(data) {
-    var img = data.primaryImage;
-    var artistName = data.artistDisplayName;
-    var artTitle = data.title;
-    var objectUrl = data.objectURL;
+ function imgDisplay(data) {
+        var img = data.primaryImage;
+        var artistName = data.artistDisplayName;
+        var artTitle = data.title;
+        var objectUrl = data.objectURL;
 
-    document.querySelector("#apiImg").src = img;
-    document.querySelector(".artistName").innerText = artistName;
-    document.querySelector(".artTitle").innerText = artTitle;
+        document.querySelector("#apiImg").src = img; 
+        document.querySelector(".artistName").innerText = artistName;
+        document.querySelector(".artTitle").innerText = artTitle;
    
     //clicking on the image will bring you to the url//
 
-    $("#apiImg").on('click', function(){
+    $("#apiImg").on('click', function() {
         window.location = objectUrl;    
     });
 }
 
 document.addEventListener("DOMContentLoaded",fetchObjects());
-//document.addEventListener("DOMContentLoaded",fetchImage());
+
 
 
